d71207da8d548a2bb3fbea0bf4a671f937885259
aryba <arybacka@interia.eu>
2024-06-22 09:05:51 +0200

    zadania na rozmowe rekrutacyjna

---
+++ src/Palindrom.java
@@ -0,0 +1,21 @@
+package src;
+
+public class Palindrom {
+
+    //kajak
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom("kajak"));
+    }
+
+    public static boolean isPalindrom(String word) {
+        int n = word.length();
+        for (int i = 0; i < (n / 2); i++) {
+            if (word.charAt(i) != word.charAt(n - i - 1)) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
